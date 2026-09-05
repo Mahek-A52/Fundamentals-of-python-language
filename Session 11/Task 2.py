54cabc97d8c6d36f44e82ece5d8e5f7b8bd6bd97
Mahek-A52 <mahekganchi9@gmail.com>
2026-09-05 15:40:04 +0530

    Create Task 2.py

---
+++ Session 11/Task 2.py
@@ -0,0 +1,13 @@
+my_playlist = {
+    "Shape of You": 4.2,
+    "Perfect": 4.4,
+    "Believer": 3.2
+}
+
+# Add a new song
+my_playlist["Blinding Lights"] = 3.5
+
+# Update an existing song
+my_playlist["Perfect"] = 4.5
+
+print(my_playlist)
